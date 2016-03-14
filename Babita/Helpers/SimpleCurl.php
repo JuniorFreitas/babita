448d6563580b4aef4fc2d26d4bfc34e60ff5dead
Fábio Assunção <fabio@fabioassuncao.com.br>
2016-03-14 19:45:05 -0300

    Simplified syntax for array in Helpers

--- Babita/Helpers/SimpleCurl.php
+++ Babita/Helpers/SimpleCurl.php
@@ -30,19 +30,19 @@ class SimpleCurl
     *
     * @return string returns the content of the given url
     */
-    public static function get($url, $params = array())
+    public static function get($url, $params = [])
     {
         if (is_array($params) && count($params) > 0) {
             $url = $url . '?' . http_build_query($params, '', '&');
         }
         $ch = curl_init();
 
-        $options = array(
+        $options = [
             CURLOPT_URL => $url,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_CONNECTTIMEOUT => 10,
             CURLOPT_SSL_VERIFYPEER => false
-        );
+        ];
         curl_setopt_array($ch, $options);
 
         $response = curl_exec($ch);
@@ -60,11 +60,11 @@ class SimpleCurl
     *
     * @return string returns the content of the given url after post
     */
-    public static function post($url, $fields = array())
+    public static function post($url, $fields = [])
     {
         $ch = curl_init();
 
-        $options = array(
+        $options = [
             CURLOPT_URL => $url,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_CONNECTTIMEOUT => 10,
@@ -72,7 +72,7 @@ class SimpleCurl
             CURLOPT_POSTFIELDS => $fields,
             CURLOPT_POST => true,
             CURLOPT_USERAGENT => USER_AGENT,
-        );
+        ];
         curl_setopt_array($ch, $options);
 
         $response = curl_exec($ch);
@@ -90,18 +90,18 @@ class SimpleCurl
     *
     * @return string with the contents of the site
     */
-    public static function put($url, $fields = array())
+    public static function put($url, $fields = [])
     {
         $post_field_string = http_build_query($fields);
         $ch = curl_init($url);
 
-        $options = array(
+        $options = [
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_CONNECTTIMEOUT => 10,
             CURLOPT_SSL_VERIFYPEER => false,
             CURLOPT_CUSTOMREQUEST => "PUT",
             CURLOPT_POSTFIELDS => $post_field_string
-            );
+        ];
         curl_setopt_array($ch, $options);
 
         $response = curl_exec($ch);
