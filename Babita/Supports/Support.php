f52c622744f5a80fe0f34c616b209d11f3d04bb2
Fábio Assunção <fabio@fabioassuncao.com.br>
2016-03-14 17:17:24 -0300

    Framework Supports

---
+++ Babita/Supports/Support.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Launcher to support functions
+ *---------------------------------------------------------------------------------------
+ * @author Fábio Assunção da Silva - fabioassuncao.com
+ * @version 1.0
+ * @date February 08, 2016
+ *---------------------------------------------------------------------------------------
+ */
+
+use Babita\Core\View;
+use Babita\Helpers\Date;
+use Babita\Helpers\Url;
+
+require ('I18n.php');
+require ('View.php');
+require ('Date.php');
+require ('Data.php');
+require ('Url.php');
+require ('Security.php');
