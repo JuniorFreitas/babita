1dfd9df855ef86106d7f29125bb371912c5fb2a2
Fábio Assunção <fabio@fabioassuncao.com.br>
2016-03-14 17:10:40 -0300

    index and bootstrap application

---
+++ bootstrap.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * @author Fábio Assunção - fabio@fabioassuncao.com.br
+ * @version 1.0
+ * @date February 06, 2016
+ *
+ * BABITA SMVC specifed directory default is '.'
+ * If app folder is not in the same directory update it's path
+ */
+$babita = '.';
+
+define('DS', DIRECTORY_SEPARATOR);
+
+/** Set the full path to the docroot */
+define('ROOT', realpath(dirname(__FILE__)).DS);
+
+/** Make the application relative to the docroot, for symlink'd index.php */
+if (!is_dir($babita) and is_dir(ROOT.$babita)) {
+    $babita = ROOT.$babita;
+}
+
+/** Define the absolute paths for configured directories */
+define('BABITA', realpath($babita).DS);
+unset($babita);
+
+/** Load configs files */
+
+$filesConfig = include_once "configs.php";
+
+foreach ($filesConfig as $file) {
+    if (file_exists(BABITA.'config'.DS.$file)) {
+        require (BABITA.'config'.DS.$file);
+    }else{
+        throw new \Exception("No {$file} found, configure {$file} in config folder.");
+    }
+}
+
+/** load composer autoloader */
+if (file_exists(BABITA .'vendor'.DS.'autoload.php')) {
+    require (BABITA.'vendor'.DS.'autoload.php');
+} else {
+    throw new \Exception('Please install via composer.json');
+}
+
+/*
+ *---------------------------------------------------------------
+ * ERROR REPORTING
+ *---------------------------------------------------------------
+ *
+ * Different environments will require different levels of error reporting.
+ * By default development will show errors but production will hide them.
+ */
+
+
+if (defined('ENVIRONMENT')) {
+    switch (ENVIRONMENT) {
+        case 'development':
+            error_reporting(E_ALL);
+            break;
+        case 'production':
+            error_reporting(0);
+            break;
+        default:
+            exit('The application environment is not set correctly.');
+    }
+
+}
+
+/**
+ * Turn on output buffering.
+ */
+ob_start();
+
+/**
+ * Turn on custom error handling.
+ */
+set_exception_handler('Babita\Core\Logger::ExceptionHandler');
+set_error_handler('Babita\Core\Logger::ErrorHandler');
+
+/**
+ * Set timezone.
+ */
+date_default_timezone_set(TIMEZONE);
+
+/**
+ * Start sessions.
+ */
+Babita\Helpers\Session::init();
+
---
+++ configs.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author Fábio Assunção - fabio@fabioassuncao.com.br
+ * @version 1.0
+ * @date March 14, 2016
+ * Set additional files of the framework configuration
+ */
+return [
+    'config.php',
+    'session.php',
+    'database.php',
+    'email.php'
+];
