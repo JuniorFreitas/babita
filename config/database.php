3e8d8c63d122ca4f6869c44f4f994efa17d3bc26
Fábio Assunção <fabio@fabioassuncao.com.br>
2016-03-14 17:12:39 -0300

    config database

---
+++ config/database.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @author Fábio Assunção - fabio@fabioassuncao.com.br
+ * @version 1.0
+ * @date February 06, 2016
+ *
+ * Database details ONLY NEEDED IF USING A DATABASE
+ *
+ */
+
+/**
+ * Database engine default is mysql.
+ */
+define('DB_TYPE', 'mysql');
+
+/**
+ * Database host default is localhost.
+ */
+define('DB_HOST', 'localhost');
+
+/**
+ * Database name.
+ */
+define('DB_NAME', 'dbname');
+
+/**
+ * Database username.
+ */
+define('DB_USER', 'root');
+
+/**
+ * Database password.
+ */
+define('DB_PASS', 'password');
+
+/**
+ * Database port.
+ */
+define('DB_PORT', '3306');
+
+/**
+ * PREFER to be used in database calls default is BABITA_
+ */
+define('DB_PREFIX', 'BABITA_');
