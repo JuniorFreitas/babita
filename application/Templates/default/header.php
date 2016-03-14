73e781e98803d434c5b90f22b2c52af677065358
Fábio Assunção <fabio@fabioassuncao.com.br>
2016-03-14 17:04:36 -0300

    templates application directory

---
+++ application/Templates/default/header.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Sample layout
+ */
+
+use Babita\Helpers\Assets;
+use Babita\Helpers\Url;
+use Babita\Helpers\Hooks;
+
+//initialise hooks
+$hooks = Hooks::get();
+?>
+<!DOCTYPE html>
+<html lang="<?=LANGUAGE_CODE;?>">
+<head>
+
+	<!-- Site meta -->
+	<meta charset="utf-8">
+	<?php
+	//hook for plugging in meta tags
+	$hooks->run('meta');
+	?>
+	<title><?=$data['title'].' - '.SITETITLE; //SITETITLE defined in config/config.php ?></title>
+
+	<!-- CSS -->
+	<?php
+	Assets::css(array(
+		'//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css',
+		Url::base() . '/css/style.css',
+	));
+
+	//hook for plugging in css
+	$hooks->run('css');
+	?>
+
+</head>
+<body>
+<?php
+//hook for running code after body tag
+$hooks->run('afterBody');
+?>
+
+<div class="container">
