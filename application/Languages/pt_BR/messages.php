883bdce40406d31e533c1cd5604b76b8c13ecec0
Fábio Assunção <fabio@fabioassuncao.com.br>
2016-03-14 17:05:51 -0300

    languages application directory

---
+++ application/Languages/pt_BR/messages.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Exemplo de tradução
+ */
+return [
+
+	'welcome_text' => 'Olá,',
+	'welcome_message' => '
+		Olá, bem-vindos a partir do controller de boas-vindas <br/>
+		Este conteúdo pode ser alterado no arquivo <code>/application/Views/welcome/welcome.php</code>
+	',
+
+	'subpage_text' => 'Subpágina',
+	'subpage_message' => '
+		Esta página está sendo executada a partir do método de controller de boas-vindas de subpágina! <br/>
+		Este conteúdo pode ser alterado no arquivo <code>/application/Views/welcome/subpage.php</code>
+	',
+
+	'open_subpage' => 'Abrir subpágina',
+	'back_home' => 'Início',
+
+];
